10cd1f1b7a58fc7ced04ea6d2b68e8dd33fc7eeb
OlgaTikhonchik <olga882011@gmail.com>
2023-06-08 16:50:36 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -5,10 +5,11 @@ const contactsSlice = createSlice({
   name: 'contacts',
   initialState: initialContacts,
   reducers: {
-    addContact(state, action) {
-      state.push(action.payload);
+    addContact: (state, action) => {
+      // state.contacts.push(action.payload);
+      return [...state, action.payload];
     },
-    deleteContact(state, action) {
+    deleteContact: (state, action) => {
       const index = state.findIndex(contact => contact.id === action.payload);
       state.splice(index, 1);
     },
